2233341b0d768e689e59bf0b9e7420de1637cb8c
Guilherme <104802974+GuilhermeAdSilva@users.noreply.github.com>
2026-09-09 07:19:13 -0300

    feat: adição de templates e páginas

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+export function NotFound() {
+
+  return (
+    <MainTemplate>
+        <Container>
+            <h1>Página não encontrada</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum sed in, dolore soluta reiciendis aut molestiae et possimus quisquam inventore blanditiis repellendus nulla dolor porro, nihil qui voluptates minima non.</p>
+        </Container>
+    </MainTemplate>
+  );
+}
